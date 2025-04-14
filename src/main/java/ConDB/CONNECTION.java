107afa9d194731fd4cc3ae80fd3b546d8fdfc248
NgDinh2k <ngdinh0511@gmail.com>
2025-04-14 11:11:06 +0700

    connectDB

---
+++ src/main/java/ConDB/CONNECTION.java
@@ -0,0 +1,28 @@
+package ConDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CONNECTION {
+    public static Connection getConnection() {
+        try {
+            String URL = "jdbc:sqlserver://localhost:1433;DatabaseName=QLKHO;user=sa;password=123;"
+                    + "encrypt=true; trustServerCertificate=true;sslProtocol=TLSv1.2";
+            Connection con = DriverManager.getConnection(URL);
+            return con;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        Connection conn = CONNECTION.getConnection();
+        if (conn != null) {
+            System.out.println("Kết nối thành công từ hàm main!");
+        } else {
+            System.out.println("Kết nối thất bại từ hàm main.");
+        }
+    }
+}
+
+    
