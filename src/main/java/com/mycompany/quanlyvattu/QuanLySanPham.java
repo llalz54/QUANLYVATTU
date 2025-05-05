9ddd068e8a40d17afb4f8a96c8a793cd82c91ce5
llalz54 <vanducthanh0209@gmail.com>
2025-05-05 15:21:28 +0700

    sua qlsp

--- src/main/java/com/mycompany/quanlyvattu/QuanLySanPham.java
+++ src/main/java/com/mycompany/quanlyvattu/QuanLySanPham.java
@@ -98,7 +98,7 @@ public class QuanLySanPham extends javax.swing.JFrame {
     }
 
     private String convertStatus(String status) {
-<<<<<<< HEAD
+
         switch (status) {
             case "1":
                 return "Đã bán";
@@ -106,21 +106,12 @@ public class QuanLySanPham extends javax.swing.JFrame {
                 return "Tồn kho";
             default:
                 return "Tất cả";
-=======
-    switch (status) {
-        case "1":
-            return "Đã bán";
-        case "0":
-            return "Tồn kho";
-        default:
-            return "Tất cả";
-    }
 }
+    }
 
    private void loadDataTableSPTheoTrangThai() {
     String selected = cbLocSP.getSelectedItem().toString();
     ArrayList<SANPHAM> dssp = null;
-
     switch (selected) {
         case "Tất cả":
             dssp = sp_data.getListSP();
@@ -147,43 +138,9 @@ public class QuanLySanPham extends javax.swing.JFrame {
             vec.add(sp.getEndDate());
 
             dtm.addRow(vec);
->>>>>>> grandmaster_TS
         }
     }
-
-    private void loadDataTableSPTheoTrangThai() {
-        String selected = cbLocSP.getSelectedItem().toString();
-        ArrayList<SANPHAM> dssp = null;
-
-        switch (selected) {
-            case "Tất cả":
-                dssp = sp_data.getListSP();
-                break;
-            case "Tồn kho":
-                dssp = sp_data.getlistSP_TK();
-                break;
-            case "Đã bán":
-                dssp = sp_data.getlistSP_DB();
-                break;
-        }
-
-        DefaultTableModel dtm = (DefaultTableModel) tbSP.getModel();
-        dtm.setRowCount(0); // Xóa dữ liệu cũ
-
-        if (dssp != null) {
-            for (SANPHAM sp : dssp) {
-                Vector<Object> vec = new Vector<>();
-                vec.add(sp.getProductID());
-                vec.add(sp.getTenLoai());
-                vec.add(sp.getSerial());
-                vec.add(convertStatus(sp.getStatus()));
-                vec.add(sp.getStartDate());
-                vec.add(sp.getEndDate());
-
-                dtm.addRow(vec);
-            }
-        }
-    }
+   }
 
     private void loadTable_TheoSerial(String Serial) {
         DefaultTableModel dtm = (DefaultTableModel) tbSP.getModel();
